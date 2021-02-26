89e23395c8906db253b93f2c2e16914cf3c555a8
wangzhichao <willwaywang6@gmail.com>
2021-02-26 15:34:04 +0800

    review.

--- Language-tour/_07_Control_flow_statements/_05_Switch_and_case.dart
+++ Language-tour/_07_Control_flow_statements/_05_Switch_and_case.dart
@@ -7,6 +7,8 @@ void main() {
   doByTime('dawn');
   print("-" * 20);
   doByTime('evening');
+
+  print(getWeekDayString(4));
 }
 
 void doByTime(String time) {
@@ -33,5 +35,26 @@ void doByTime(String time) {
       break;
     default:
       print('Unknown time');
+  }
+}
+
+String getWeekDayString(int weekday) {
+  switch (weekday) {
+    case 1:
+      return "星期一";
+    case 2:
+      return "星期二";
+    case 3:
+      return "星期三";
+    case 4:
+      return "星期四";
+    case 5:
+      return "星期五";
+    case 6:
+      return "星期六";
+    case 7:
+      return "星期日";
+    default:
+      throw "输入错误";
   }
 }
